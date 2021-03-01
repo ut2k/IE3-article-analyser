9484e37ecc57fa763e6847bbd5fd70866215bbdb
Marco Wang <marcowang01@gmail.com>
2021-03-02 03:36:26 +0800

    90%

--- text_classification/ibc_classification.py
+++ text_classification/ibc_classification.py
@@ -22,7 +22,6 @@ import time
 
 
 def main():
-    print("\nCleaning json...")
     clean_json('../released_data.json', '../Dataset/cleaned_data.json')  # uncomment to ues full dataset
     # clean_json('../Dataset/test_data.json', '../Dataset/cleaned_data.json') # comment to use smaller dataset
 
@@ -36,30 +35,11 @@ def main():
     return
 
 
-def classify_out_of_sample(name, path, vectorizer, vec_name):
-    article = pd.read_json(path)
-    # transform new article into doc-term matrix
-    article_counts = vectorizer.transform(article['content'])
-    # run trained model on new article
-    filename = "Models/" + vec_name + "_" + name + ".sav"
-    loaded_clf = pickle.load(open(filename, 'rb'))
-    # predicted = loaded_clf.predict(article_counts) # predict class
-    predicted = loaded_clf.predict_proba(article_counts)  # predict probabilities
-
-    for i in range(len(predicted)):
-        # print("\t\"" + article['title'][i][:20] + "...\" : " + predicted[i]) # print class
-        proba_str = F"L: {predicted[i][1]:.2%} | R: {predicted[i][2]:.2%} | C: {predicted[i][0]:.2%}"
-        print(F"\t\"{article['title'][i][:10]}...\" | " + proba_str)  # print probabilities
-
-    return
-
-
-REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
-BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
-STOPWORDS = set(stopwords.words('english'))
-
-
 def clean_text(text):
+    REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+    BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+    STOPWORDS = set(stopwords.words('english'))
+
     text = text.lower()  # lowercase text
     text = REPLACE_BY_SPACE_RE.sub(' ', text)  # replace REPLACE_BY_SPACE_RE symbols by space in text
     text = BAD_SYMBOLS_RE.sub('', text)  # delete symbols which are in BAD_SYMBOLS_RE from text
@@ -67,11 +47,10 @@ def clean_text(text):
     return text
 
 
-# use pipeline to normalize document term matrix
 def ibc_classify(data: pd.DataFrame):
     print("First 5 docs:")
     for i in range(5):
-        print(F"{i + 1}: {data['content'][i][:50]}...")
+        print(F"{i + 1}: {data['content'][i][:75]}...")
 
     # print("\nClean text...")
     # data['content'] = data['content'].apply(clean_text)
@@ -87,13 +66,14 @@ def ibc_classify(data: pd.DataFrame):
     text_counts = cv.fit_transform(tqdm(data['content']))  # returns a sparse matrix, entry = matrix[x, y]
     M_SIZE = text_counts.shape
     DISPLAY_INDEX = 2901508  # index for the word "people"
+    I_RANGE = range(DISPLAY_INDEX - 2, DISPLAY_INDEX + 3)
     print(F"Matrix size: {M_SIZE}")
 
     feature_list = cv.get_feature_names()
-    print(F"Ex. features: {feature_list[DISPLAY_INDEX : DISPLAY_INDEX + 5]}")
-    print(F"Ex. features in first 5 docs:")
+    print(F"Select features: {feature_list[DISPLAY_INDEX : DISPLAY_INDEX + 5]}")
+    print(F"Select features in first 5 docs:")
     for i in range(5):
-        for j in range(DISPLAY_INDEX, DISPLAY_INDEX + 5):
+        for j in I_RANGE:
             print(F"\t{text_counts[i, j]}", end=" ")
         print()
     # turn feature_list into a dict with the index as value -> random access
@@ -102,10 +82,11 @@ def ibc_classify(data: pd.DataFrame):
 
     NEU_LEN, LIB_LEN, CON_LEN = 14846, 4448, 4448
     ROW_LEN = M_SIZE[0]
-    UNI_FACTOR, BI_FACTOR, TRI_FACTOR = 5, 1.5, .75
+    UNI_FACTOR, BI_FACTOR, TRI_FACTOR = 2.5, .5, .25
     VEC_ID = F"{UNI_FACTOR}{BI_FACTOR}{TRI_FACTOR}"
+
     print('\nIntegrating IBC data...')
-    DO_IBC_INTEGRATION = False
+    DO_IBC_INTEGRATION = True
     if DO_IBC_INTEGRATION:
         with open("./../Dataset/ibc_data/feature_lists/neu_list.csv", 'r') as f:
             reader = csv.DictReader(f)
@@ -198,9 +179,9 @@ def ibc_classify(data: pd.DataFrame):
     else:
         filename = F'Vectorizers/{VEC_ID}_cv.sav'
         text_counts = pickle.load(open(filename, 'rb'))
-    print(F"Ex. features in first 5 docs:")
+    print(F"Select features in first 5 docs:")
     for i in range(5):
-        for j in range(DISPLAY_INDEX, DISPLAY_INDEX + 5):
+        for j in I_RANGE:
             print(F"\t{text_counts[i, j]}", end=" ")
         print()
 
@@ -214,21 +195,21 @@ def ibc_classify(data: pd.DataFrame):
         filename = F'Vectorizers/{VEC_ID}_tfidf.sav'
         tfidf_counts = pickle.load(open(filename, 'rb'))
 
-    # tfidf_counts = text_counts
-    print(F"Ex. features in first 5 docs:")
+    tfidf_counts = text_counts
+    print(F"Select features in first 5 docs:")
     for i in range(5):
-        for j in range(DISPLAY_INDEX, DISPLAY_INDEX + 5):
-            print(F"\t{tfidf_counts[i, j]:.2}", end=" ")
+        for j in I_RANGE:
+            print(F"\t{float(tfidf_counts[i, j]):.2}", end=" ")
         print()
 
     X_train, X_test, y_train, y_test = train_test_split(
         tfidf_counts, data['allsides_bias'], test_size=0.3, random_state=123)
 
     print(F"\nTraining set:")
-    print(F"Ex. features in first 5 docs:")
+    print(F"Select features in first 5 docs:")
     for i in range(5):
-        for j in range(DISPLAY_INDEX, DISPLAY_INDEX + 5):
-            print(F"\t{X_train[i, j]:.2}", end=" ")
+        for j in I_RANGE:
+            print(F"\t{float(X_train[i, j]):.2}", end=" ")
         print()
     print(F"First 5 tags: \n{y_train[:5]}")
 
@@ -238,7 +219,7 @@ def ibc_classify(data: pd.DataFrame):
     start = time.time()
     clf = AdaBoostClassifier().fit(X_train, y_train)
     end = time.time()
-    print(F"elapsed time: {end - start}")
+    print(F"elapsed time: {(end - start)/60:.2} min")
     name = "AdaBoostClassifier"
     y_pred = clf.predict(X_test)
     log_new()
@@ -268,6 +249,7 @@ def log_new():
 
 
 def clean_json(path_r: str, path_w: str):
+    print("\nCleaning json...")
     with open(path_r, 'r') as f_r:
         with open(path_w, 'w') as f_w:
             count = 0
